a56a8c673366075c3150c3ec50f7945e42400ba7
rpapara <r.papara@gmail.com>
2026-03-26 22:47:51 +0300

    4.3.3 Задание: независимость от данных

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,13 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+links = ("http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+         )
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize("link", links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
